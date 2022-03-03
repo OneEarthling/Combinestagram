48288b1322089d938c8dba53d5a35863ae5fedb2
nadya <kazynkina@keysystems.ru>
2022-03-03 11:31:23 +0300

    challenge 2

--- Combinestagram/MainViewController.swift
+++ Combinestagram/MainViewController.swift
@@ -105,8 +105,28 @@ class MainViewController: UIViewController {
   }
 
   func showMessage(_ title: String, description: String? = nil) {
-    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
-    present(alert, animated: true, completion: nil)
+    alert(title: title, text: description)
+//      .subscribe(onCompleted: {
+//        print("completed")
+//      }, onError: { [weak self] error in
+//        print("error")
+//      })
+      .subscribe()
+      .disposed(by: bag)
+  }
+}
+
+extension UIViewController {
+  func alert(title: String, text: String?) -> Completable {
+    return Completable.create { [weak self] completable in
+      let alertVC = UIAlertController(title: title, message: text, preferredStyle: .alert)
+      alertVC.addAction(UIAlertAction(title: "Close", style: .default, handler: {_ in
+        completable(.completed)
+      }))
+      self?.present(alertVC, animated: true, completion: nil)
+      return Disposables.create {
+        self?.dismiss(animated: true, completion: nil)
+      }
+    }
   }
 }
